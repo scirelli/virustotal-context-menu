34939f03fef01d34d569e077ff36b45f48dd5d6b
Steve Cirelli <scirelli+git@gmail.com>
2024-09-03 15:59:48 -0400

    quick save

--- context.sh
+++ context.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
-notify-send \
-    --app-name='QuickScan' \
-    --category='transfer.complete' \
-    --icon=dialog-information \
-    'Virustotal' "$* $(/media/scirelli/Chromebook/scirelli/Projects/scirelli/virustotal-context-menu/test.sh "$@")"
+
+appName='QuickScan'
+title='Virustotal Scan'
+files=("$@")
+results=$(/media/scirelli/Chromebook/scirelli/Projects/scirelli/virustotal-context-menu/test.sh "${files[@]}")
+
+for file in "${files[@]}"; do
+    fileName=$(basename -- "$file")
+
+    notify-send \
+        --app-name="$appName" \
+        --category='transfer.complete' \
+        --icon=dialog-information \
+        "$title of $fileName" "$results"
+done
